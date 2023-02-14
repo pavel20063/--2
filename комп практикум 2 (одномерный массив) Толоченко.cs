4c636e3af2bd293d388426b1bb1be99576a0b11d
pavel20063 <123302689+pavel20063@users.noreply.github.com>
2023-02-14 11:29:13 +0200

    Add files via upload

---
+++ комп практикум 2 (одномерный массив) Толоченко.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace ConsoleApp4
+   
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {   //ЗАДАНИЕ 1
+            //int[] work1 = new int[] { 5, 8, 3, 1, 7, 5 };
+            //int firstElement = work1[0];
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    if (work1[i] == firstElement)
+            //    {
+            //        Console.WriteLine("Элемент со значением {0} найден. Его порядковый номер: {1}", firstElement, i);
+            //    }
+            //}
+
+
+            //ЗАДАНИЕ 2
+            //int[] work2 = new int[] { 85, 200, 35, 120, 90, 80, 150 };
+
+            //Console.WriteLine("Элементы не превыщающие 100:");
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (work2[i] <= 100)
+            //    {
+            //        Console.Write(work2[i] + " ");
+            //    }
+            //}
+
+            //ЗАДАНИЕ 3
+            //int[] work3 = { 5, -3, 4, -8, -9, 1, 9, 6, -3, 4, 8, 2 };
+            //int count = 0;
+            //foreach (int num in work3)
+            //{
+            //    if (num < 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+
+           //ЗАДАНИЕ 4
+            //int[] work4 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //int sum = 0;
+            //int count = 0;
+
+            //for (int i = 0; i < work4.Length; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        sum += work4[i];
+            //        count++;
+            //    }
+            //    double average = (double)sum / count;
+            //    Console.WriteLine("Среднее арифметическое элементов с нечетными номерами:" + average);
+            //}
+            
+
+            //ЗАДАНИЕ 5
+            //int[] work5 = { 10, 20, 30, 40, 50 }; 
+
+            //for (int i = 0; i < work5.Length; i++)
+            //{
+            //    work5[i] *= 5;
+            //Console.ReadLine();
+
+
+        }
+
+
+
+    }
+
+}
